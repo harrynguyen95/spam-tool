3322919a698bb0f17f2033276c67e57bb2d4f8e4
Harry Nguyen <38271443+harrynguyen95@users.noreply.github.com>
2025-07-10 19:09:05 +0700

    up

--- database/migrations/2015_06_29_01_create_device_table.php
+++ database/migrations/2015_06_29_01_create_device_table.php
@@ -11,12 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('devices', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('ip_address');
-            $table->timestamps();
-        });
+        if (! Schema::hasTable('devices')) {
+            Schema::create('devices', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('ip_address');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
