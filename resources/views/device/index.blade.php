80d004888efe27bb2a1ff70f9c39fc3d669cec0f
Harry Nguyen <38271443+harrynguyen95@users.noreply.github.com>
2025-07-23 22:43:36 +0700

    up

--- resources/views/device/index.blade.php
+++ resources/views/device/index.blade.php
@@ -222,7 +222,7 @@
                             </select>
                         </div>
 
-                        <div class="col-md-3 d-flex align-items-center" style="height: 45px;">
+                        <div class="col-md-3 d-flex align-items-center hidden" style="height: 45px;">
                             <label class="text-end" style="margin-right: 10px;">Change info (VN)</label>
                             <div class="form-check form-check-inline me-3">
                                 <input class="form-check-input" type="radio" name="change_info" id="change_info_yes" value="1" {{ $config->change_info == '1' ? 'checked' : '' }}>
